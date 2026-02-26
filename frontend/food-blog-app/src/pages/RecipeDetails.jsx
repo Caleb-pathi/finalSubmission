17c76b69354ddff87d255aca0fe65c303d6fa084
Caleb-pathi <pathicaleb2050@gmail.com>
2026-02-27 01:12:59 +0530

    Updated frontend API URLs for deployment

--- frontend/food-blog-app/src/pages/RecipeDetails.jsx
+++ frontend/food-blog-app/src/pages/RecipeDetails.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import profileImg from '../assets/profile.png'
-import food from '../assets/foodRecipe.png'
-import { useLoaderData } from 'react-router-dom'
+import React from "react";
+import profileImg from "../assets/profile.png";
+import food from "../assets/foodRecipe.png";
+import { useLoaderData } from "react-router-dom";
+
+const API = import.meta.env.VITE_API_URL;
 
 export default function RecipeDetails() {
-    const recipe=useLoaderData()
-    console.log(recipe)
+  const recipe = useLoaderData();
+
   return (
-   <>
-    <div className='outer-container'>
-        <div className='profile'>
-            <img src={profileImg} width="50px" height="50px"></img>
-            <h5>{recipe.email}</h5>
+    <>
+      <div className="outer-container">
+        <div className="profile">
+          <img src={profileImg} width="50px" height="50px" alt="profile" />
+          <h5>{recipe.email}</h5>
         </div>
-        <h3 className='title'>{recipe.title}</h3>
-        <img src={`http://localhost:5000/images/${recipe.coverImage}`} width="220px" height="200px"></img>
-        <div className='recipe-details'>
-            <div className='ingredients'><h4>Ingredients</h4><ul>{recipe.ingredients.map(item=>(<li>{item}</li>))}</ul></div>
-            <div className='instructions'><h4>Instructions</h4><span>{recipe.instructions}</span></div>
+
+        <h3 className="title">{recipe.title}</h3>
+
+        <img
+          src={`${API}/images/${recipe.coverImage}`}
+          width="220px"
+          height="200px"
+          alt="recipe"
+        />
+
+        <div className="recipe-details">
+          <div className="ingredients">
+            <h4>Ingredients</h4>
+            <ul>
+              {recipe.ingredients.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="instructions">
+            <h4>Instructions</h4>
+            <span>{recipe.instructions}</span>
+          </div>
         </div>
-    </div>
-   </>
-  )
+      </div>
+    </>
+  );
 }
